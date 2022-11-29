44133a562167a58fe42d87a7e0da3bfce4b4393a
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-29 17:51:31 -0300

    feat: add font sizes and variants

---
+++ src/theme/fonts.ts
@@ -0,0 +1,44 @@
+export const FONT_WEIGHT = {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+    extraBlack: 950,
+}
+
+export const FONT_SIZES = {
+    largeTitle: 40,
+    h1: 30,
+    h2: 22,
+    h3: 16,
+    h4: 14,
+    h5: 12,
+    body1: 30,
+    body2: 22,
+    body3: 16,
+    body4: 14,
+    body5: 12,
+};
+
+export const HEADING_SIZES = {
+    largeTitle: { 'font-size': FONT_SIZES.largeTitle, 'font-weight': FONT_WEIGHT.bold },
+    h1: { 'font-size': FONT_SIZES.h1, 'font-weight': FONT_WEIGHT.bold },
+    h2: { 'font-size': FONT_SIZES.h2, 'font-weight': FONT_WEIGHT.bold },
+    h3: { 'font-size': FONT_SIZES.h3, 'font-weight': FONT_WEIGHT.bold },
+    h4: { 'font-size': FONT_SIZES.h4, 'font-weight': FONT_WEIGHT.bold },
+    h5: { 'font-size': FONT_SIZES.h5, 'font-weight': FONT_WEIGHT.bold },
+};
+
+export const TYPOGRAPHY_SIZES = {
+    largeTitle: { 'font-size': FONT_SIZES.largeTitle, 'font-weight': FONT_WEIGHT.bold },
+    body1: { 'font-size': FONT_SIZES.body1 },
+    body2: { 'font-size': FONT_SIZES.body2 },
+    body3: { 'font-size': FONT_SIZES.body3 },
+    body4: { 'font-size': FONT_SIZES.body4 },
+    body5: { 'font-size': FONT_SIZES.body5 },
+};
