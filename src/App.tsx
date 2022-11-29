54c7f49f8dc4373938c51907e10335c12e242b73
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-29 17:57:45 -0300

    feat: add the themeprovider in root

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
+import Heading from './components/Heading';
+import { defaultTheme } from './theme/themeColors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <div className="App">
+        <header className="App-header">
+          <Heading variant="h1" level={1}>
+            Quiz APP
+          </Heading>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
