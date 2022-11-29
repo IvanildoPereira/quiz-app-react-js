825a27807f0fd5808c8340788e4b9f4b9bd92c16
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-29 18:22:37 -0300

    setup: example questions to test

---
+++ src/data/questions.ts
@@ -0,0 +1,85 @@
+export interface subject{
+    subject: string;
+    theme: {
+        themeName: string,
+        questions: questionProps[]
+    };
+} 
+
+export interface questionProps{
+    questionId: number;
+    question: string;
+    options: string[];
+    answer: string;
+}
+
+export const subjects: subject[] = [
+    {
+        subject: "Historia",
+        theme: {
+            themeName: "1° Guerra Mundial",
+            questions: [
+                {
+                    questionId: 1,
+                    question: "(Cetrede) Ao final da Primeira Guerra Mundial, instalou-se na Alemanha uma Assembleia Nacional que se reuniu em Weimar para preparar uma nova Constituição para o país. Nascia ali um novo Estado, chamado de:",
+                    options: [
+                        "Sacro Império Romano-Germânico.",
+                        "República de Weimar.",
+                        "Confederação Germânica.",
+                        "República Democrática da Alemanha.",
+                        "República Federal da Alemanha."
+                    ],
+                    answer: "República de Weimar."
+                },
+                {
+                    questionId: 2,
+                    question: "(Avança-SP) Dos fenômenos abaixo citados, assinale aquele que é considerado como o causador da Primeira Guerra Mundial.",
+                    options: [
+                        "O assassinato da família imperial russa.",
+                        "O fim do colonialismo africano.",
+                        "A emancipação dos povos árabes.",
+                        "O descumprimento do Tratado de Versalhes por parte da Alemanha.",
+                        "O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco."
+                    ],
+                    answer: "O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austríaco."
+                },
+                {
+                    questionId: 3,
+                    question: "(Pref. Betim-MG) É coerente com as razões que levaram à 1ª Grande Guerra Mundial:",
+                    options: [
+                        "Um dos fatos que contribuíram para o final do confronto foi a entrada da Rússia na guerra, pois tinha um exército grande e bem preparado, impondo aos alemães derrotas vexatórias.",
+                        "O processo de imperialismo, promovido pelas grandes potências capitalistas da Europa, principalmente França, Inglaterra e Alemanha, gerou conflitos e até confrontos pela disputa de territórios, a ponto de desencadear a 1ª Guerra.",
+                        "Temendo uma ofensiva alemã, Japão, Inglaterra e França formaram a Tríplice Aliança.",
+                        "O início da guerra se deu quando as tropas alemãs invadiram a Polônia, apresentando ao mundo a famosa guerra-relâmpago, deixando marcas desastrosas para os poloneses.",
+                        "Nenhuma das alternativas acima."
+                    ],
+                    answer: "O processo de imperialismo, promovido pelas grandes potências capitalistas da Europa, principalmente França, Inglaterra e Alemanha, gerou conflitos e até confrontos pela disputa de territórios, a ponto de desencadear a 1ª Guerra."
+                },
+                {
+                    questionId: 4,
+                    question: "Selecione a alternativa que apresenta uma fase da Primeira Guerra Mundial.",
+                    options: [
+                        "Guerra-relâmpago",
+                        "Corrida nuclear",
+                        "Guerra de Trincheiras",
+                        "Derrota do Eixo",
+                        "Avanço do Exército Vermelho"
+                    ],
+                    answer: "Guerra de Trincheiras"
+                },
+                {
+                    questionId: 5,
+                    question: "Qual país fazia parte da Tríplice Aliança, mas se recusou a participar da guerra por esse grupo quando o conflito se iniciou em 1914?",
+                    options: [
+                        "Itália",
+                        "Áustria-Hungria",
+                        "Império Otomano",
+                        "Bulgária",
+                        "Rússia"
+                    ],
+                    answer: "Itália"
+                },
+            ]
+        }
+    }
+]
