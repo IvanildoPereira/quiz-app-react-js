6578f19cbb8793c6a13a7cd6f4084a04ca5e730a
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-29 17:50:07 -0300

    feat: add colorPallete theme
    - add color of the theme project
    - default theme and dark theme

---
+++ src/theme/themeColors.ts
@@ -0,0 +1,42 @@
+import IColorsPallete from "./IColorsPallete"
+
+export const defaultTheme: IColorsPallete = {
+    backgroundColor: "#FFFFFF",
+    onBackgroundColor: "#2B2B2B",
+    onBackgroundLowColor: "#9598A1",
+    onBackgroundMediumColor: "#707279",
+    onBackgroundHighColor: "#2B2B2B",
+    surfaceColor: "#FFFFFF",
+    onSurfaceColor: "#000000",
+    primaryColor: "#6C5CE7",
+    onPrimaryColor: "#FFFFFF",
+    secondaryColor: "#6C5CE7",
+    onSecondaryColor: "#FFFFFF",
+    successColor: "#37B26C",
+    onSuccessColor: "#ffffff",
+    errorColor: "#E62E5C",
+    onErrorColor: "#ffffff",
+    warningColor: "#FFC640",
+    onWarningColor:"#ffffff",
+}
+
+export const darkTheme: IColorsPallete = {
+    backgroundColor: "#121212",
+    onBackgroundColor: "#fff",
+    onBackgroundLowColor: "#fcfcfc",
+    onBackgroundMediumColor: "#fff",
+    onBackgroundHighColor: "#fff",
+    surfaceColor: "#1d1d1d",
+    onSurfaceColor: "#fff",
+    primaryColor: "#686868",
+    onPrimaryColor: "#ffffff",
+    secondaryColor: "#6C5CE7",
+    onSecondaryColor: "#FFFFFF",
+    successColor: "#37B26C",
+    onSuccessColor: "#ffffff",
+    errorColor: "#E62E5C",
+    onErrorColor: "#ffffff",
+    warningColor: "#FFC640",
+    onWarningColor:"#ffffff",
+}
+
