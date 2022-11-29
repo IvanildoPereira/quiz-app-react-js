c870b18a632c7f9959827210964bce971c228ad6
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-29 17:53:09 -0300

    feat: add Typography and Heading Component
    - Components that use the theme options as a design system pattern

---
+++ src/components/Heading.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { HEADING_SIZES, FONT_WEIGHT } from "../theme/fonts"
+import { defaultTheme } from "../theme/themeColors"
+
+type HeadingProps = {
+    color?: keyof typeof defaultTheme;
+    variant?: keyof typeof HEADING_SIZES;
+    fontWeight?: keyof typeof FONT_WEIGHT;
+    align?: "left" | "center" | "right",
+    decoration?: "underline" | "line-through"
+    level?: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+const Heading = styled('h1').attrs<HeadingProps>(({ level }) => ({
+    as: `h${level}`}))<HeadingProps>`
+    color: ${({ theme, color }) => color ? theme[color] : theme.primaryColor};
+    font-weight: ${props => props.fontWeight ? FONT_WEIGHT[props.fontWeight] : FONT_WEIGHT["normal"]};
+    text-align: ${props => props.align ? props.align : "left"};
+    text-decoration: ${props => props.decoration ? props.decoration : "none"};
+    ${props => props.variant && HEADING_SIZES[props.variant]};
+`;
+
+export default Heading;
---
+++ src/components/Typography.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+import { TYPOGRAPHY_SIZES, FONT_WEIGHT } from "../theme/fonts"
+import { defaultTheme } from "../theme/themeColors"
+
+interface TypographyProps{
+    color?: keyof typeof defaultTheme;
+    variant?: keyof typeof TYPOGRAPHY_SIZES;
+    fontWeight?: keyof typeof FONT_WEIGHT;
+    align?: "left" | "center" | "right",
+    decoration?: "underline" | "line-through"
+}
+
+const Typography = styled.p<TypographyProps>`
+    color: ${({ theme, color }) => color ? theme[color] : theme.primaryColor};
+    font-weight: ${props => props.fontWeight ? FONT_WEIGHT[props.fontWeight] : FONT_WEIGHT["normal"]};
+    text-align: ${props => props.align ? props.align : "left"};
+    text-decoration: ${props => props.decoration ? props.decoration : "none"};
+    ${props => props.variant && TYPOGRAPHY_SIZES[props.variant]};
+`
+
+export default Typography;
