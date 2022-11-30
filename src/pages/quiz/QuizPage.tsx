3e06d277b6976044762b6f9123c098b779677478
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-30 20:01:54 -0300

    feat: add quizPage with multistep
    - Card with quiz with multistep that allow to go back and next and keep the state

---
+++ src/pages/quiz/QuizPage.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../../components/Button";
+import Heading from "../../components/Heading";
+import Typography from "../../components/Typography";
+import { questionProps, subjects } from "../../data/questions";
+import QuestionOption from "./components/QuestionOption";
+
+interface SelectedAnswer{
+    questionId: number;
+    selectedAnswer: string | null;
+} 
+
+const QuizPage = () =>{
+    const [questions, setQuestions] = useState<questionProps[] | null>(null)
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+    const [selectedAnswers, setSelectedAnswers] = useState<SelectedAnswer[]>([]);
+    const [error, setError] = useState<string | null>(null)
+    const { themeId } = useParams();
+
+    useEffect(() =>{
+
+        let theme = subjects[0].themes.filter((theme) => {
+            return theme.themeId.toString() === themeId 
+        })
+
+        setQuestions(theme[0].questions);
+
+        let initSelectedAnswers: SelectedAnswer[] = [];
+        theme[0].questions.forEach((question) => {
+            initSelectedAnswers.push({
+                questionId: question.questionId,
+                selectedAnswer: null
+            })
+        })
+        setSelectedAnswers(initSelectedAnswers);
+    },[themeId])
+
+    const handleNextQuestion = () =>{
+        setCurrentQuestion(currentQuestion + 1);
+    }
+
+    const onSelectOption = (selectedOption: string) =>{
+        const copySelectedAnswers = [...selectedAnswers];
+        copySelectedAnswers[currentQuestion].selectedAnswer = selectedOption;
+        setSelectedAnswers(copySelectedAnswers);
+    }
+
+    const handlePreviousQuestion = () =>{
+        setCurrentQuestion(currentQuestion - 1);
+    }
+
+    const checkForNullAnswers = () =>{
+        return selectedAnswers.filter((selectedAnswer) => {
+            return selectedAnswer.selectedAnswer === null;
+        });
+    }
+
+    const createErrorMessage = (nullAnswers: SelectedAnswer[]) =>{
+        let errorText = nullAnswers.length > 1 ? "Questions" : "Question";
+        
+        nullAnswers.forEach((nullAnswer, index) =>{
+            let findedindex = selectedAnswers.findIndex((Answer) =>{
+                return nullAnswer.questionId === Answer.questionId;
+            })
+            if(nullAnswers.length < 2) errorText +=` ${findedindex + 1} must be answerd!`;
+            else if(index + 1 !== nullAnswers.length) errorText +=` ${findedindex + 1},`;
+            else errorText +=` and ${findedindex + 1} must be answerd!`
+        })
+
+        return errorText;
+    }
+
+    const handleSave = () =>{
+        setError(null);
+        let nullAnswers = checkForNullAnswers(); 
+        if( nullAnswers.length === 0){
+            return;
+        }
+
+        let errorText = createErrorMessage(nullAnswers)
+
+        setError(errorText);
+    }
+
+    return(
+        <QuestionCard>
+            {error && 
+                <ErrorContainer>
+                    <Typography color="onErrorColor" align="center" fontWeight="bold">{error}</Typography>    
+                </ErrorContainer>
+            }
+            {questions && <Heading variant="h2" level={3}>{questions[currentQuestion].question}</Heading>}
+            
+            {questions && questions[currentQuestion].options.map((option: string, index: number)=>(
+               <QuestionOption 
+                    key={index}
+                    indexItem = {index}
+                    option = {option} 
+                    isSelectedOption = {selectedAnswers[currentQuestion]?.selectedAnswer === option} 
+                    onSelectOption = {onSelectOption}
+                />
+            ))}
+            {questions && <ButtonGroup>
+                {currentQuestion !== 0 && <Button onClick={handlePreviousQuestion} background="surfaceColor" color="primaryColor" outlined>Previous</Button>}
+                {currentQuestion + 1 !== questions.length && <Button onClick={handleNextQuestion}>Next</Button>}
+                {currentQuestion + 1 === questions.length && <Button onClick={handleSave}>Save</Button>}
+            </ButtonGroup>}
+        </QuestionCard>
+    )
+}
+
+const QuestionCard = styled.div`
+    background-color: ${({ theme }) => theme.surfaceColor};
+    padding: 20px;
+    
+    border-radius: 20px;
+    box-shadow: 4px 4px 9px -3px rgba(0,0,0,0.75);
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+`
+
+const ErrorContainer = styled.div`
+    background-color: ${({ theme }) => theme.errorColor};
+    width: 100%;
+    padding: 20px;
+    margin: 20px 0;
+    border-radius: 10px;
+`;
+
+export default QuizPage;
---
+++ src/pages/quiz/components/QuestionOption.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import Typography from "../../../components/Typography";
+
+interface QuestionOptionType{
+    indexItem: number;
+    option: string;
+    isSelectedOption: boolean;
+    onSelectOption: (selectedOption: string) => void;
+}
+
+let letterOption = ["A", "B", "C", "D", "E"]
+
+const QuestionOption = ({indexItem, option, isSelectedOption, onSelectOption} : QuestionOptionType) =>{
+    return(
+        <QuestionOptionOptionContainer onClick={() => onSelectOption(option)} isSelectedOption = {isSelectedOption}>
+            <OptionLetterRounded>
+                <Typography tag="span" variant="body3" color={"onPrimaryColor"} fontWeight="bold">
+                    {letterOption[indexItem]}
+                </Typography>
+            </OptionLetterRounded>
+            <Typography className="text_question" variant="body3" color={isSelectedOption ? "onPrimaryColor" : "onSurfaceColor"} fontWeight="bold">{option}</Typography>
+        </QuestionOptionOptionContainer>
+    )
+}
+
+const QuestionOptionOptionContainer = styled.button<{
+    isSelectedOption: boolean;
+}>` 
+    display: flex;
+    align-items: center;
+    width: 100%;
+    background-color: ${({ theme, isSelectedOption }) => isSelectedOption ? theme.primaryColor : theme.surfaceColor};
+    padding: 20px;
+    margin: 15px 0;
+    cursor: pointer;
+    border: 1px solid ${({ theme }) => theme.primaryColor};
+    border-radius: 10px;
+
+    &:hover{
+        background-color: ${({ theme }) => theme.primaryColor} 
+    }
+
+    &:hover p{
+        color: ${({ theme }) => theme.onPrimaryColor}
+    }
+`;
+
+const OptionLetterRounded = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin-right: 15px;
+    background-color: ${({ theme }) => theme.primaryColor};
+    box-shadow: 2px 2px 10px -4px rgba(0,0,0,0.75)
+`
+
+export default QuestionOption;
