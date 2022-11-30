a1f4e0df24405724d4dff104d5f14dbfeefc13a3
Ivanildo Pereira <ivanildop99@gmail.com>
2022-11-30 15:12:12 -0300

    feat: add subjectPage to handle the subjects themes questions

---
+++ src/pages/subject/Subject.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import styled, { ThemeConsumer } from "styled-components";
+import Heading from "../../components/Heading";
+import Typography from "../../components/Typography";
+import { subjects, subjectProps } from "../../data/questions";
+import QuestionCard from "./components/QuestionCard";
+
+const Subject = () =>{
+    const [subject, setSubject] = useState<subjectProps |null>(null);
+    useEffect(()=>{
+        setSubject(subjects[0]);
+    },[])
+    return(
+        <SubjectContainer>
+            {subject && <Heading variant="h1" level={2}>{subject.subject} - Temas</Heading>}
+            <GridContainer>
+                {subject && subject.themes.map((theme)=>(
+                    <QuestionCard bgImage={theme.themeBG} themeName = {theme.themeName} numberOfQuestions={theme.questions.length}/>
+                ))}
+            </GridContainer>
+        </SubjectContainer>
+    )
+}
+
+const SubjectContainer = styled.div``;
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-gap: 20px;
+    margin: 20px 0;
+    @media (min-width: 768px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    
+`;
+
+export default Subject;
