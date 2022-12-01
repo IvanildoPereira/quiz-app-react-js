40a18fcd3157495db9a0d300446b7ec7214b5751
Ivanildo Pereira <ivanildop99@gmail.com>
2022-12-01 11:39:16 -0300

    feat: create result component

---
+++ src/pages/quiz/components/Result.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Result = () =>{
+    return(
+        <ResultContainer>
+
+        </ResultContainer>
+    )
+}
+
+const ResultContainer = styled.div`
+`;
+
+export default Result;
