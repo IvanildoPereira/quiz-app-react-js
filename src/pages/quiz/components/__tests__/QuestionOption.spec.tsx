ce71729b157720d706a2acfc587a948564c87980
Ivanildo Pereira <ivanildop99@gmail.com>
2022-12-01 18:05:18 -0300

    refactor: add question index to the questionOption

--- src/pages/quiz/components/__tests__/QuestionOption.spec.tsx
+++ src/pages/quiz/components/__tests__/QuestionOption.spec.tsx
@@ -7,34 +7,34 @@ const onSelect = jest.fn();
 describe("QuestionOption", () =>{
     it("should render the option", () =>{
         const expectedOption = "Guerra de Trincheiras";
-        render(<QuestionOption indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
+        render(<QuestionOption questionIndex={1} indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
 
         expect(screen.getByText(expectedOption)).toBeTruthy();
     })
 
     it("should render the letter option as B", () =>{
         const expectedOption = "Guerra de Trincheiras";
-        render(<QuestionOption indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
+        render(<QuestionOption questionIndex={1} indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
         expect(screen.getByTestId("letter-option")).toHaveTextContent("B");
     })
 
     it("should call onSelect when clicked in the function", () =>{
         const expectedOption = "Guerra de Trincheiras";
-        render(<QuestionOption indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
+        render(<QuestionOption questionIndex={1} indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
         userEvent.click(screen.getByText(expectedOption))
         expect(onSelect).toBeCalled();
     })
 
     it("should return false if option is not selected", () =>{
         const expectedOption = "Guerra de Trincheiras";
-        render(<QuestionOption indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
+        render(<QuestionOption questionIndex={1} indexItem={1} option={expectedOption} isSelectedOption={false} onSelectOption={onSelect}/>);
         const option = screen.getByTestId("question-option")
         expect(option.getAttribute("data-selected")).toBe("false");
     })
 
     it("should return true if option is selected", () =>{
         const expectedOption = "Guerra de Trincheiras";
-        render(<QuestionOption indexItem={1} option={expectedOption} isSelectedOption={true} onSelectOption={onSelect}/>);
+        render(<QuestionOption questionIndex={1} indexItem={1} option={expectedOption} isSelectedOption={true} onSelectOption={onSelect}/>);
         const option = screen.getByTestId("question-option")
         expect(option.getAttribute("data-selected")).toBe("true");
     })
